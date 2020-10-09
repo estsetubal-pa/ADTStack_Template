7e936fccc00dcd9f9afb258c00b7a25c3ef5dc24
brunomnsilva <brunomnsilva@gmail.com>
2020-10-09 16:14:23 +0100

    Linked list base implementation final.

--- src/pt/pa/adts/StackLinkedList.java
+++ src/pt/pa/adts/StackLinkedList.java
@@ -4,21 +4,33 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class StackLinkedList<T> implements Stack<T> {
 
-    private Node top; //sentinel
+    private Node top; //sentinel of linked list
     private int size;
 
     public StackLinkedList() {
+        /*
+        The configuration of an empty singly linked list is the sentinel
+        pointing to null.
+         */
         this.top = new Node(null, null);
         this.size = 0;
     }
 
     @Override
     public void push(T element) throws FullStackException {
+        /*
+        Element should be stored in a new node which is pointed by top sentinel.
+        Must maintain the chaining of all nodes.
+         */
         throw new NotImplementedException();
     }
 
     @Override
     public T pop() throws EmptyStackException {
+        /*
+        The element at the top of the stack is the element stored in the
+        node immediately after the top sentinel.
+         */
         throw new NotImplementedException();
     }
 
@@ -42,6 +54,10 @@ public class StackLinkedList<T> implements Stack<T> {
         throw new NotImplementedException();
     }
 
+    /**
+     * Inner class representing a linked list node.
+     * Only recognized in the context of this class (private).
+     */
     private class Node {
         private T element;
         private Node next;
